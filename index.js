0e6ef70a2ce72d00aa8ed8ef0572b0d9ea631d46
Rob Hess <hessro@oregonstate.edu>
2021-10-21 15:32:47 -0700

    Add code from 10/21 lecture.

--- index.js
+++ index.js
@@ -48,3 +48,89 @@ console.log("== str1[3]:", str1[3])
 console.log("== str1.replace('s', 'x'):", str1.replace('s', 'x'))
 console.log("== 'decathlon'.indexOf('cat'):", 'decathlon'.indexOf('cat'))
 console.log("== 'team'.indexOf('i'):", 'team'.indexOf('i'))
+
+console.log("====================")
+console.log("== Functions")
+console.log("====================")
+
+function addStuff(a, b, c) {
+  // console.log("== In addStuff, arguments:", arguments)
+  // if (arguments.length !== 3) {
+  //
+  // }
+  // if (!c) {
+  //   console.log("== c is falsy")
+  // }
+  return a + b + c
+}
+
+console.log("== addStuff(1, 2, 3):", addStuff(1, 2, 3))
+console.log("== addStuff('1', '2', '3'):", addStuff('1', '2', '3'))
+console.log("== addStuff(1, 2, '3'):", addStuff(1, 2, '3'))
+console.log("== typeof(addStuff):", typeof(addStuff))
+console.log("== typeof(undefined):", typeof(undefined))
+console.log("== addStuff(1, 2):", addStuff(1, 2))
+console.log("== addStuff(1, 2, 3, 4, 5, 6, 7):", addStuff(1, 2, 3, 4, 5, 6, 7))
+
+var foo = addStuff;
+console.log("== foo(1, 2, 3):", foo(1, 2, 3))
+
+var bar = function () {
+  console.log("== Inside our anonymous function")
+}
+bar()
+
+console.log("====================")
+console.log("== Arrays")
+console.log("====================")
+
+var array = [ 1, '2', foo, [ 'a', 'b', 'c' ] ]
+console.log("== array:", array)
+console.log("== typeof(array):", typeof(array))
+console.log("== array.length:", array.length)
+console.log("== array.indexOf('2'):", array.indexOf('2'))
+console.log("== array.indexOf(3):", array.indexOf(3))
+
+for (var i = 0; i < array.length; i++) {
+  console.log("== array[" + i + "]:", array[i])
+}
+
+function printArrayElement(elem) {
+  console.log("== In printArrayElement(), elem:", elem, ", arguments[1]:", arguments[1])
+}
+
+array.forEach(printArrayElement)
+
+array.forEach(function (blah, anotherArg) {
+  console.log("== In forEach(), blah:", blah, "anotherArg:", anotherArg)
+})
+
+console.log("====================")
+console.log("== Object")
+console.log("====================")
+
+var mapping = {
+  'cat': 'cute',
+  'dog': 'loyal',
+  'fish': 'boring'
+}
+console.log("== mapping:", mapping)
+console.log("== mapping['cat']:", mapping['cat'])
+
+var student = {
+  name: 'Luke Skywalker',
+  firstName: 'Luke',
+  lastName: 'Skywalker',
+  gpa: 3.75,
+  email: 'TheLastJedi@hotmail.com',
+  getFullName: function () {
+    return this.firstName + " " + this.lastName
+  }
+}
+console.log("== student:", student)
+console.log("== student['name']:", student['name'])
+console.log("== student.name:", student.name)
+
+var field = 'gpa'
+console.log("== student[field]:", student[field])
+console.log("== student.getFullName():", student.getFullName())
